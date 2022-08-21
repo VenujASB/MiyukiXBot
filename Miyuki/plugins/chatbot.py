3c0cf9e75f000f03c8f390069843a104644ac445
VenujASB <109645464+VenujASB@users.noreply.github.com>
2022-08-21 13:33:39 +0530

    `done`

--- Miyuki/plugins/chatbot.py
+++ Miyuki/plugins/chatbot.py
@@ -1,15 +1,14 @@
 import requests
 from pyrogram import filters
+from pyrogram.types import Message
 from googletrans import Translator
-from Miyuki import *
-from Miyuki.core.decorators.permissions import adminsOnly
-from Miyuki.utils.filter_groups import *
+from Miyuki import app,BOT_ID
+from Miyuki.utils.filter_groups import cbot
 from lang import get_command
-from Miyuki.utils.lang import *
-from Miyuki.utils.commands import *
-from Miyuki import *
+from Miyuki.utils.lang import language
 from Miyuki.mongo import chatb
 from Miyuki.plugins.antlangs import get_arg
+from Miyuki.utils.custom_filters import admin_filter
 
 
 tr = Translator()
@@ -17,18 +16,13 @@ CBOT = get_command("CBOT")
 CBOTA = get_command("CBOTA")
 
 
-@app.on_message(
-    filters.command("chatbot")
-    & ~filters.edited
-    & ~filters.private
-)
-@adminsOnly("can_delete_messages")
+@app.on_message(filters.command("chatbot") & ~filters.edited& ~filters.private& admin_filter)
 @language
 async def cbots(client, message: Message, _):
     group_id = str(message.chat.id)
     chat_id = message.chat.id
     user_id = message.from_user.id
-    user = await bot.get_chat_member(group_id, user_id)
+    user = await app.get_chat_member(group_id, user_id)
     if not user.status == "creator" or user.status == "administrator":
         return
     if len(message.command) < 2:
@@ -39,22 +33,14 @@ async def cbots(client, message: Message, _):
     sex = await message.reply_text(_["antil2"])
     lower_args = args.lower()
     if lower_args == "on":
-        chatb.insert_one({f"chatbot": group_id})#default AI is Afflicate+
+        chatb.insert_one({f"chatbot": group_id})
     elif lower_args == "off":
         chatb.delete_one({f"chatbot": group_id})
     else:
         return await sex.edit(_["chatb1"])
     await sex.edit(f"✅ **Successfully** `{'Enabled' if lower_args=='on' else 'Disabled'}` ** Chat bot**")
 
-@app.on_message(
-    filters.text
-    & filters.reply
-    & ~filters.bot
-    & ~filters.via_bot
-    & ~filters.forwarded
-    & ~filters.private
-    & ~filters.edited,
-    group=cbot)
+@app.on_message(filters.text & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded & ~filters.private & ~filters.edited, group=cbot)
 async def szcbot(_, message: Message):
     chat_id = message.chat.id
     if not message.reply_to_message:
@@ -67,48 +53,49 @@ async def szcbot(_, message: Message):
         return
     chat = chatb.find_one({"chatbot":chat_id})   
     if chat:
-       await app.send_chat_action(message.chat.id, "typing")
-       lang = tr.translate(message.text).src
-       trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
-       text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
-       affiliateplus = requests.get(f"https://api.affiliateplus.xyz/api/chatbot?message={text}&botname=Miyuki&ownername=@VenujaST&user=1")
-       textmsg = (affiliateplus.json()["message"])
-       if "Affiliate+" in textmsg:
-        textmsg = textmsg.replace("Affiliate+", "Miyuki bot created by @VenujaST")
-       if "Lebyy_Dev" in textmsg:
-        textmsg = textmsg.replace("Lebyy_Dev", "@VenujaST")
-       if "God Brando" in textmsg:
-        textmsg = textmsg.replace("God Brando", f"{message.from_user.first_name}")
-       if "seeker" in textmsg:
-        textmsg = textmsg.replace("seeker", f"wow")
-       msg = tr.translate(textmsg, src='en', dest=lang)
-       await message.reply_text(msg.text)
-
-
-async def lunaQuery(query: str, user_id: int):
-    luna = await arq.luna(query, user_id)
-    return luna.result
-
-
-
+        await app.send_chat_action(message.chat.id, "typing")
+        try:
+           lang = tr.translate(message.text).src
+           trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
+           text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
+           affiliateplus = requests.get(f"https://api.affiliateplus.xyz/api/chatbot?message={text}&botname=Miyuki&ownername=@venujast&user={chat_id}")
+           textmsg = (affiliateplus.json()["message"])
+           if "Affiliate+" in textmsg:
+               textmsg = textmsg.replace("Affiliate+", "Miyuki")
+           if "Lebyy_Dev" in textmsg:
+               textmsg = textmsg.replace("Lebyy_Dev", "Venuja Sadew")
+           if "God Brando" in textmsg:
+               textmsg = textmsg.replace("God Brando", f"{message.from_user.first_name}")
+           if "seeker" in textmsg:
+               textmsg = textmsg.replace("seeker", f"wow")
+           msg = tr.translate(textmsg, src='en', dest=lang)
+           await message.reply_text(msg.text)
+        except Exception:
+           user_id = message.from_user.id
+           lang = tr.translate(message.text).src
+           trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
+           text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
+           safeone = requests.get(f"https://api.safone.tech/chatbot?message={text}&bot_name=Miyuki&bot_master=Venuja&user_id={user_id}")
+           textmsg = (safeone.json()["answer"])
+           if "Affiliate+" in textmsg:
+               textmsg = textmsg.replace("Affiliate+", "Miyuki")
+           if "[Safone]" in textmsg:
+               textmsg = textmsg.replace("[Safone]", "Venuja Sadew")
+           msg = tr.translate(textmsg, src='en', dest=lang)
+           await message.reply_text(msg.text)
 
 __MODULE__ = "Chat Bot"
 __HELP__ = """
 **Chatbot**
-
 AI based chatbot allows rose to talk and provides a more interactive group chat experience.
-
 - /chatbot [ON/OFF]: Enables and disables Affiliate + AI Chat bot.
-
-
 **Available chatbots**
 • Luna - Advanced, inteligent and cute chatbot which will keep you happy all time.. 
-
-
 **Language Support**
-Miyuki AI chatbot support almost all languages in world .
+Rose AI chatbot support almost all languages in world .
 Powered By ; `googletrans==3.1.0a0`
 """
+
 
 
 
